a8046affae0a0b6abbd9961dd5f9e2b66fb35ec7
oncescuandreea <etgeniuand@yahoo.com>
2020-09-30 17:17:42 +0100

    Added a new accuracy function that generates the euclidean matrix distance between image and text codes and picks the lowest distances as being coupled

--- model/metric.py
+++ model/metric.py
@@ -1,5 +1,23 @@
 import torch
 
+def pairwise_distances(x, y=None):
+    '''
+    Input: x is a Nxd matrix
+           y is an optional Mxd matirx
+    Output: dist is a NxM matrix where dist[i,j] is the square norm between x[i,:] and y[j,:]
+            if y is not given then use 'y=x'.
+    i.e. dist[i,j] = ||x[i,:]-y[j,:]||^2
+    obtained from https://discuss.pytorch.org/t/efficient-distance-matrix-computation/9065
+    '''
+    x_norm = (x**2).sum(1).view(-1, 1)
+    if y is not None:
+        y_norm = (y**2).sum(1).view(1, -1)
+    else:
+        y = x
+        y_norm = x_norm.view(1, -1)
+
+    dist = x_norm + y_norm - 2.0 * torch.mm(x, torch.transpose(y, 0, 1))
+    return dist
 
 def accuracy_tot(output, target, no_tasks):
     correct = 0
@@ -21,6 +39,17 @@ def accuracy(output, target):
         correct = 0
         correct += torch.sum(pred == target).item()
     return correct / len(target)
+
+def accuracy_retrieval(output, target):
+    with torch.no_grad():
+        # import pdb; pdb.set_trace()
+        batch_len = len(target)
+        dist = pairwise_distances(output, target)
+        min_dist_location = dist.argmin(1)
+        tensor_idx = torch.tensor(range(0, batch_len)).cuda()
+        bool_correct_min_location = (min_dist_location == tensor_idx).tolist()
+        correct = bool_correct_min_location.count(True)
+    return correct / batch_len
 
 def top_k_acc(output, target, k=3):
     with torch.no_grad():
