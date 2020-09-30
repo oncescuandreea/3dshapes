f600130a09af29c0dd2af6af7aa338c3d78c1532
oncescuandreea <etgeniuand@yahoo.com>
2020-09-30 17:16:46 +0100

    Added a retrieval triple loss partially manually implemented

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,7 @@
 import torch.nn.functional as F
-from torch.nn import MSELoss
+from torch.nn import MSELoss, MarginRankingLoss, TripletMarginLoss
+import numpy as np
+import torch
 
 
 
@@ -9,3 +11,33 @@ def nll_loss(output, target):
 def mse_loss(output, target):
     loss = MSELoss()
     return loss(output, target.double())
+def retrieval_loss(output, target):
+    return MarginRankingLoss(output, target)
+def retrieval_loss2(output, target, no_negatives: int):
+    """Function returns triplet loss obtained from comparing the anchor
+    with the corresponding postivie label and with another no_negatives
+    negative examples"""
+    triplet_loss = TripletMarginLoss(margin=1.0, p=2)
+    batch_len = len(target)
+    batch_loss = 0
+    negative_idx = np.random.randint(0, batch_len, np.min([batch_len, no_negatives]))
+
+    total_examples = no_negatives * batch_len
+    batch_example_dim = len(output[0])
+    list_tensors = []
+    # for target_example in target:
+    #     list_tensors.append(target_example.reshape([1, batch_example_dim]))
+    # target_reshaped = torch.cat(list_tensors)
+    # import pdb; pdb.set_trace()
+    for k, output_example in enumerate(output):
+        output_example_reshaped = output_example.reshape([1, batch_example_dim])
+        for idx in negative_idx:
+            if idx != k:
+                try:
+                    batch_loss += triplet_loss(output_example_reshaped, target[k], target[idx])
+                except IndexError:
+                    import pdb; pdb.set_trace()
+            else:
+                total_examples -= 1
+    return batch_loss / total_examples
+
